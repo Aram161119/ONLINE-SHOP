cbc668f5511a4a9c7ab2645e010ee726fae47f51
Aram <hrayryan77@gmail.com>
2026-01-13 15:21:25 +0400

    Add loadEnv in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 3000,
-		proxy: {
-			'/api': {
-				target: import.meta.env.VITE_API_URL || 'http://localhost:5000',
-				changeOrigin: true,
-				rewrite: (p) => p.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	const apiUrl = env.VITE_API_URL || 'http://localhost:5000';
+
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
 			},
 		},
-	},
+		server: {
+			host: '0.0.0.0',
+			port: 3000,
+			proxy: {
+				'/api': {
+					target: apiUrl,
+					changeOrigin: true,
+					rewrite: (p) => p.replace(/^\/api/, ''),
+				},
+			},
+		},
+	};
 });
