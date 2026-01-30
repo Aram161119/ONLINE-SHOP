955c1d9a90f3c7afb3e87cfd486b5a1dc8a10e20
Aram <hrayryan77@gmail.com>
2026-01-30 16:52:58 +0400

    Replace last commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-	const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+	const env = loadEnv(mode, process.cwd(), '');
+	const API_URL = env.VITE_API_URL || 'http://localhost:5000';
 
 	return {
 		base: '/',
